a8527c8dbaba316d5a9d81cd941918120ea687c2
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-13 14:45:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-// Redirect to login page
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if yes then redirect to dashboard
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: dashboard.php");
+    exit;
+}
+
+// Redirect to login page if not logged in
 header("Location: login.php");
 exit();
 ?> 

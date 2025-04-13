faed7b7cf4c7790b344e4fc22c9c890028bc2d8d
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-13 15:53:25 +0530

    Update view_policy.php

--- view_policy.php
+++ view_policy.php
@@ -174,7 +174,7 @@ include 'includes/header.php';
                                         <td><?php echo date('d M Y H:i', strtotime($doc['created_at'])); ?></td>
                                         <td>
                                             <?php 
-                                            $file_path = $upload_dir . $doc['file_name'];
+                                            $file_path = $upload_dir . $doc['file_path'];
                                             if(file_exists($file_path)): 
                                             ?>
                                             <a href="<?php echo $file_path; ?>" 
